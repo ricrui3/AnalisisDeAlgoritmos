ce3088d9401fe3cf48e3e29cf0cfaa185cde1970
ricrui3 <ricrui3@gmail.com>
2015-11-05 19:33:15 -0600

    shells

--- MultiplicacionDeMatrices/TamaniosMatrices/scritpTamanio.sh
+++ MultiplicacionDeMatrices/TamaniosMatrices/scritpTamanio.sh
@@ -5,6 +5,6 @@ n=50
 
 for (( i = 0; i < "$n"; i++ )); do
 		valor_n=$(python -c "print 2 ** $i")
-		python tamanio.py $valor_n > $valor_n.txt
+		python tamanio.py ${valor_n} > ${valor_n}.txt
 		echo "$valor_n"
 done
