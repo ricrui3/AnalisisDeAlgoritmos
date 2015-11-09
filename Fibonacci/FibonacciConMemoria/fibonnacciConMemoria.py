abaeff9122eba5e7dda49431613abeb6353fb568
ricrui3 <ricrui3@gmail.com>
2015-11-09 16:20:28 -0600

    cambios en fibonnaciConMemoria

--- Fibonacci/FibonacciConMemoria/fibonnacciConMemoria.py
+++ Fibonacci/FibonacciConMemoria/fibonnacciConMemoria.py
@@ -6,12 +6,12 @@ def fibonacciSimple(n):
     if n == 0 or n == 1:
         return n
     else:
-        if (M[n - 1] != "E") and (M[n - 2] != "E"):
-            M[n] = M[n - 1] + M[n - 2]
-            return M[n]
-        else:
-            M[n] = fibonacciSimple(n - 1) + fibonacciSimple(n - 2)
-            return M[n]
+        if (M[n - 1] == "E"):
+            M[n - 1] = fibonacciSimple(n - 1)
+        if (M[n - 2] == "E"):
+            M[n - 1] = fibonacciSimple(n - 2)
+        M[n] = M[n - 1] + M[n - 2]
+        return M[n]
 
 
 def inicio():
