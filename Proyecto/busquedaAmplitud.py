49f64b401126edb6bbc57af9faa2c3b3b07048c0
Erick Bravo <erickfabian_bravo@hotmail.com>
2016-01-15 19:57:51 -0600

    amplitud

--- Proyecto/busquedaAmplitud.py
+++ Proyecto/busquedaAmplitud.py
@@ -1,27 +1,45 @@
 def bfs(graph, start):
-    nodosVisitados, cola = set(), [start]
-    while cola:
-        nodo = cola.pop(0)
-        if nodo not in nodosVisitados:
-            nodosVisitados.add(nodo)
-            cola.extend(graph[nodo] - nodosVisitados)
-    return nodosVisitados
+    path = []
+    queue=[start]
+    while queue:
+        v=queue.pop(0) # pops out the first in (go wider)
+        if v not in path:
+            path+=[v]
+            queue+=graph[v] # push v's neighbors to queue
+    return path
 
-def crear_matriz_dict(M, n):
-    aux = set()
+def crear_matriz_dict_costos(Mc, Mg, n):
+    auxG = set()
+    auxC = []
     for i in range(n):
         strAux = input().split()
         for j in range(n):
             iaux = int(strAux[j])
+            auxC.append(iaux)
             if iaux > 0:
-                aux.add(j)
-        M[i] = aux.copy()
-        aux.clear()
-    return M
+                auxG.add(j)
+        Mg[i] = auxG.copy()
+        Mc.append(list(auxC[0: n]))
+        auxC.clear()
+        auxG.clear()
+    return Mg
 
+
+def costo(nodosV, matriz_costos):
+    cost = 0
+    for i in range(len(nodosV) - 1):
+        cost += matriz_costos[nodosV[i]][nodosV[i + 1]]
+    return cost
 
 n = int(input())
 matriz_grafo = {}
-crear_matriz_dict(matriz_grafo, n)
+matriz_costos = []
+crear_matriz_dict_costos(matriz_costos, matriz_grafo, n)
+print(matriz_costos)
+print(matriz_grafo)
 nodoInicial = int(input())
-print(bfs(matriz_grafo, nodoInicial))
+nodoFinal = int(input())
+nodosVisitados = bfs(matriz_grafo, nodoInicial)
+print(nodosVisitados)
+cost = costo(nodosVisitados, matriz_costos)
+print("El costo de la primer ruta encontrada fue " + str(cost))
